4b3028226c6450bec2ccfbb41dcc1d6abd98b16d
HorizonPoint528 <91239895+HorizonPoint528@users.noreply.github.com>
2023-09-12 00:13:39 -0400

    added rank and file number

---
+++ src/board.js
@@ -0,0 +1,20 @@
+import './board.css';
+import { getCharacter } from './helper';
+
+const Board = () => {
+
+    const ranks = Array(8).fill().map((x,i) => 8-i)
+    const files = Array(8).fill().map((x,i) => getCharacter(i))
+
+    return <div className='board'>
+        <div className='tiles'>
+            {ranks.map((rank, i) =>
+                files.map((file,j) =>
+                <div>{rank}{file}</div>
+            )
+        )}
+        </div>
+    </div>
+}
+
+export default Board
